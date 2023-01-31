88b916feeaac08515fb1da815c640c4e91a482c6
Shr4528 <Shr4528@gmail.com>
2023-01-31 11:51:20 +0200

    commit

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { Router } from "express";
 
 
 
-import { addTodo, toggleTodoDone, getAllTodos } from "../controller/todo-controller.js";
+
+import { addTodo, toggleTodoDone, getAllTodos, updateTodo, deleteTodo } from "../controller/todo-controller.js";
 
 const route = express.Router();
 
 route.post("/todos", addTodo);
 route.get("/todos", getAllTodos);
 route.get("/todos/:id", toggleTodoDone)
-
+route.put("/todos/:id", updateTodo)
+route.delete("/todos/:id", deleteTodo)
 
 export default route;
